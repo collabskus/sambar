169ecd104d93d4f332ffa3e559f3f7e24364a91d
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-09-19 19:28:58 +0530

    plain1 bg

--- Src/WidgetPacks/Plain1/.config.cs
+++ Src/WidgetPacks/Plain1/.config.cs
@@ -1,19 +1,19 @@
-﻿public class BaseConfig : Config 
+﻿public class BaseConfig : Config
 {
-    public BaseConfig() 
-    { 
-        this.height = 20;
-        this.width = 0;
-        this.marginXLeft = 0;
-        this.marginXRight = 0;
-        this.marginYTop = 0;
-        this.paddingXLeft = 0;
-        this.paddingXRight = 0;
-        this.paddingYTop = 0;
-        this.paddingYDown = 0;
-        this.backgroundColor = "#282828";
-        this.borderColor = "#ffffff";
-        this.borderThickness = 0;
-        this.roundedCorners = false;
-    }
+	public BaseConfig()
+	{
+		this.height = 20;
+		this.width = 0;
+		this.marginXLeft = 0;
+		this.marginXRight = 0;
+		this.marginYTop = 0;
+		this.paddingXLeft = 0;
+		this.paddingXRight = 0;
+		this.paddingYTop = 0;
+		this.paddingYDown = 0;
+		this.backgroundColor = "#1a1a1a";
+		this.borderColor = "#ffffff";
+		this.borderThickness = 0;
+		this.roundedCorners = false;
+	}
 }
