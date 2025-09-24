d7fcbbc9ed715fc0a345b1c39c648f2c9a74ef5d
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-09-24 19:53:24 +0530

    createRelease

--- Scripts/createRelease.ps1
+++ Scripts/createRelease.ps1
@@ -8,7 +8,7 @@ dotnet publish -r win-x64
 rm -Recurse -Force -Confirm:$false bin\Release\net9.0-windows10.0.26100.0\win-x64\publish\WidgetPacks
 cp -r WidgetPacks bin\Release\net9.0-windows10.0.26100.0\win-x64\publish\WidgetPacks
 cp -r bin\Release\net9.0-windows10.0.26100.0\win-x64\publish\* ..\builds
-Compress-Archive -Path ..\builds\* -DestinationPath ..\zips\sambar.zip
+Compress-Archive -Path ..\builds\* -DestinationPath ..\zips\sambar.zip -Force
 
 # self-contained
 # dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained -c Release
